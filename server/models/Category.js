6ffba1b8eda9a0bf9219dafc15438f718c1e0240
sarah0703 <sarah.a.b.a.t.o.r.a.b@gmail.com>
2023-10-25 17:36:33 +0200

    Testing the branch

---
+++ server/models/Category.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema(
+{
+    category_name: {
+        type: String,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        default: false
+    }
+}
+)
