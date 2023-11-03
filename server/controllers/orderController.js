0d70e422f79986135957ee211b704554092ebc94
eroniste <ilyass.sboul.12@gmail.com>
2023-11-03 16:11:45 +0100

    update the status of order

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -240,3 +240,86 @@ router.delete('/ordersInit/:id', async (req, res, next) => {
       res.status(500).json({ error: 'Error initializing orders' });
     }
   });
+
+  router.put('/confirmOrder/:id', async (req, res, next) => {
+    const orderId = req.params.id;
+  
+    try {
+      // Find the order by its ID
+      const order = await Order.findById(orderId);
+  
+      if (!order) {
+        return res.status(404).json({ error: 'Order not found' });
+      }
+  
+      // User confirms the order
+      order.ConfirmedOrderId.status = true;
+  
+      // Check if both initOrderId and ConfirmedOrderId are true
+      if (order.initOrderId.status && order.ConfirmedOrderId.status) {
+        // Update the status of the order to "pending"
+        order.status = "pending";
+      }
+  
+      // Save the changes to the database
+      await order.save();
+  
+      res.send('Order confirmed successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error confirming the order' });
+    }
+  });
+  
+  router.put('/finishOrder/:id', async (req, res, next) => {
+    const orderId = req.params.id;
+  
+    try {
+      // Find the order by its ID
+      const order = await Order.findById(orderId);
+  
+      if (!order) {
+        return res.status(404).json({ error: 'Order not found' });
+      }
+  
+      // User finishes the order
+      order.finishedOrderId.status = true;
+  
+      // Save the changes to the database
+      await order.save();
+  
+      res.send('Order finished successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error finishing the order' });
+    }
+  });
+  
+  router.put('/confirmDelivery/:id', async (req, res, next) => {
+    const orderId = req.params.id;
+  
+    try {
+      // Find the order by its ID
+      const order = await Order.findById(orderId);
+  
+      if (!order) {
+        return res.status(404).json({ error: 'Order not found' });
+      }
+  
+      // Customer confirms the delivery
+      order.DeliveredOrderId.status = true;
+      if (order.finishedOrderId.status && order.DeliveredOrderId.status) {
+        // Update the status of the order to "pending"
+        order.status = "finished";
+      }
+  
+      // Save the changes to the database
+      await order.save();
+  
+      res.send('Delivery confirmed successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error confirming the delivery' });
+    }
+  });
+  
